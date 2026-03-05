689952198f784a90bfff999a457be2e17580cdbe
Matthew <cyrilmartinmj@gmail.com>
2026-03-05 12:49:52 -0500

    added background process initialization to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_background/flutter_background.dart';
 import 'screens/home_screen.dart';
 
 void main() {
+  _backgroundInitialize();
   runApp(const C25KApp());
+}
+
+_backgroundInitialize() async {
+  final androidConfig = FlutterBackgroundAndroidConfig(
+    notificationTitle: "C25K Runner",
+    notificationText: "Running in the background",
+    notificationImportance: AndroidNotificationImportance.normal,
+    notificationIcon: AndroidResource(
+      name: 'background_icon',
+      defType: 'drawable',
+    ),
+  );
+  return await FlutterBackground.initialize(androidConfig: androidConfig);
 }
 
 class C25KApp extends StatelessWidget {
