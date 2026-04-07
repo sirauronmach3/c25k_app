18cff6f80d9d84dfad1e9364543c923eeb573342
Matthew <cyrilmartinmj@gmail.com>
2026-04-07 13:51:58 -0400

    created cadence screen

---
+++ lib/screens/cadence_screen.dart
@@ -0,0 +1,9 @@
+/// Screen for setting a cadence of run and walk intervals.
+/// Allows selecting the run and walk intervals seperately, and also optionally the total duration of the workout.
+/// The values are set at the bottom of the screen, and a start button is provided to start the workout with the selected values.
+///
+/// The cadence is displayed in the top half of the screen, the color of the screen changes based on the current interval (run, walk, or waiting)
+/// Waiting : default background color
+/// Run : Red background color
+/// Walk : Green background color
+///
