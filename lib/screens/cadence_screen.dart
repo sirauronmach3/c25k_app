39c4d00c7dcd903da9c3d1e63f41766e37c96dc1
Matthew <cyrilmartinmj@gmail.com>
2026-04-09 11:24:00 -0400

    placeholder

--- lib/screens/cadence_screen.dart
+++ lib/screens/cadence_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 /// Screen for setting a cadence of run and walk intervals.
 /// Allows selecting the run and walk intervals seperately, and also optionally the total duration of the workout.
 /// The values are set at the bottom of the screen, and a start button is provided to start the workout with the selected values.
@@ -7,3 +9,17 @@
 /// Run : Red background color
 /// Walk : Green background color
 ///
+
+class CadenceScreen extends StatefulWidget {
+  const CadenceScreen({Key? key}) : super(key: key);
+
+  @override
+  _CadenceScreenState createState() => _CadenceScreenState();
+}
+
+class _CadenceScreenState extends State<CadenceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
